f8110024ea751fdc3afc23dc3c7c32fb43a02c36
Alexis Cooper <peco6819@colorado.edu>
2023-07-23 20:11:55 -0600

    Finished

--- Code_Components/src/index.js
+++ Code_Components/src/index.js
@@ -8,6 +8,7 @@ const pgp = require('pg-promise')();
 const bodyParser = require('body-parser');
 const session = require('express-session'); 
 const bcrypt = require('bcrypt'); 
+const http = require('http');
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -89,6 +90,68 @@ app.post('/register', async (req, res) => {
 
 });
 
+// Added these two from my branch -Alexis
+app.get('/check_username', function(req, res) {
+
+  var username = req.query.username;
+
+  const query = `SELECT * FROM player WHERE username = '${username}';`
+
+     db.any(query)
+
+  .then(function(data)
+  {
+     if(data.length == 0)
+     {
+        res.status(200).json({
+           status: 'available',
+           message: 'Username is available.',
+           })
+     }
+     else{
+        res.status(200).json({
+        status: 'taken',
+        message: 'Username is taken.',
+        })
+     }
+  })
+
+  .catch(function(err){
+    return console.log(err);
+  });
+})
+
+app.get('/check_email', function(req, res) {
+
+  var email = req.query.email;
+
+  const query = `SELECT * FROM player WHERE email = '${email}';`
+
+     db.any(query)
+
+  .then(function(data)
+  {
+     if(data.length == 0)
+     {
+        res.status(200).json({
+           status: 'available',
+           message: 'Email is not used by an existing account.',
+           })
+     }
+     else{
+        res.status(200).json({
+        status: 'taken',
+        message: 'Email is used by an existing account.',
+        })
+     }
+  })
+
+  .catch(function(err){
+    return console.log(err);
+  });
+})
+
+
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   try 
@@ -121,16 +184,176 @@ const auth = (req, res, next) => {
 
 app.use(auth);
 
-app.get('/table', auth, (req, res) => {
-  const bet = parseInt(req.query.betAmount);
-  console.log("bet is:",bet);
-  res.render('pages/game',{
-    user:req.session.user.username,
-    contact:req.session.user.email,
-    tokens:req.session.user.total_chips,
-    bet:bet,
-  }); 
+app.get('/table', auth, async (req, res) => {
+  // This is being replaced with a different system based on page loads
+  // try {
+  //   // Check if the user has an active session
+  //   if (req.session.sessionId) {
+  //     // Update the end_time of the existing session
+  //     const endTime = moment().format();
+  //     await db.none('UPDATE session SET end_time = $1 WHERE id = $2', [endTime, req.session.sessionId]);
+  //   } else {
+  //     // Insert a new session entry
+  //     const startTime = moment().format();
+  //     const result = await db.one('INSERT INTO session (player_id, start_time) VALUES ($1, $2) RETURNING id', [req.session.user.id, startTime]);
+  //     req.session.sessionId = result.id; // Store the session ID in the session object
+  //   }
+
+  if(req.session){
+    const session_id = parseInt(req.query.session_id)
+    const bet = parseInt(req.query.betAmount);
+    console.log("bet is:",bet);
+    console.log(session_id);
+    console.log(session_id + bet);
+
+    res.render('pages/game',{
+      user:req.session.user.username,
+      contact:req.session.user.email,
+      tokens:req.session.user.total_chips,
+      bet:bet,
+      session_id: session_id,
+    }); 
+  }
+  // } catch (error) {
+  //   console.log('Error:', error);
+  else{
+    res.redirect('/login');
+  }
 });
+
+/*------------
+Table APIs below
+------------*/
+
+app.post('/table/begin_session', function(req, res) {
+  var player_id = req.session.user.id;
+  var start_time = new Date(Date.now()).toISOString();
+
+  const query = `INSERT INTO session (id, player_id, start_time, end_time) VALUES (DEFAULT, '${player_id}', '${start_time}', NULL);`
+  console.log(query)
+  db.any(query)
+
+  .then(function(data)
+  {
+     res.status(200).json({
+        status: 'success',
+        message: 'Began session.',
+        session_id: data.id
+     })
+  })
+  .catch(function(err){
+     return console.log(err);
+  })
+})
+
+app.get('/table/get_current_session', function(req, res)
+{
+   var player_id = req.session.user.id;
+
+   const query = `SELECT * FROM session WHERE player_id = ${player_id} AND end_time IS NULL;`
+   console.log(query);
+   
+   test = db.one(query)
+      .then(function(data)
+      {
+         res.status(200).json({
+            status: 'success',
+            session_id: data.id
+         })
+      })
+      .catch(function(err){
+         console.log(err);
+      });
+});
+
+app.post('/table/update_session', function(req, res) {
+  var session_id = req.body.session_id;
+  var end_time = new Date(Date.now()).toISOString();
+
+  console.log("updated session");
+  const query = `UPDATE session SET end_time = '${end_time}' WHERE id = '${session_id}';`
+  db.any(query)
+
+  .then(function(data)
+  {
+     res.status(200).json({
+        status: 'success',
+        message: 'Ended session.',
+     })
+  })
+  .catch(function(err){
+     return console.log(err);
+  })
+})
+
+app.post('/table/add_hand', function(req, res) {
+  var session_id = req.body.session_id;
+  var player_id = req.session.user.id;
+  var bet_amount = req.body.bet_amount;
+  var is_winner = req.body.is_winner;
+
+  const query = `INSERT INTO hand (id, session_id, player_id, bet_amount, is_winner) VALUES (DEFAULT, '${session_id}', '${player_id}', '${bet_amount}', '${is_winner}') RETURNING id;`
+
+  db.any(query)
+
+  .then(function(data)
+  {
+     res.status(200).json({
+        status: 'success',
+        message: 'Added hand to database.',
+        hand_id: data[0].id
+     });
+  })
+  .catch(function(err){
+     return console.log(err);
+  })
+})
+
+app.post('/table/add_card_to_hand', function(req, res) {
+  var hand_id = req.body.hand_id;
+  var suit = req.body.suit;
+  var rank = req.body.rank;
+  var dealer_hand = req.body.dealer_hand;
+
+  const query = `INSERT INTO card (id, hand_id, suit, rank, dealer_hand) VALUES (DEFAULT, '${hand_id}', '${suit}', '${rank}', '${dealer_hand}');`
+
+  db.any(query)
+
+  .then(function(data)
+  {
+     res.status(200).json({
+        status: 'success',
+        message: 'Added card to database.',
+     })
+  })
+  .catch(function(err){
+     return console.log(err);
+  })
+})
+
+app.post('/table/update_winner', function(req, res) {
+  var hand_id = req.body.hand_id;
+
+  const query = `UPDATE hand SET is_winner = 1 WHERE id = ${hand_id};`
+
+  db.any(query)
+  .then(function()
+  {
+    res.status(200).json({
+      status: 'OK',
+      message: 'Updated database'
+    })
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+})
+
+/*------------
+Table APIs above
+------------*/
+
+
 
 app.get('/profile', auth, (req, res) => {
   res.render('pages/profile',{
@@ -154,6 +377,7 @@ app.get('/update_tokens',auth,(req,res)=>{
   var tokenz = parseInt(req.query.tokenz);
   var bet = parseInt(req.query.bet);
   var page = parseInt(req.query.acchion);
+  var session = parseInt(req.query.session_id);
   var sign = req.query.sign;
   if(sign==0){tokenz=tokenz-bet};
   if(sign==1){tokenz=tokenz+bet};
@@ -163,7 +387,7 @@ app.get('/update_tokens',auth,(req,res)=>{
   req.session.user.total_chips = tokenz;
   console.log("total:",tokenz,"bet:",bet,"math",sign,"page:",page);
   if(tokenz>500 && page==3){
-    res.redirect('/bet');
+    res.redirect('/bet?session=' + session);
   }
   else(res.redirect('/profile'))
 });
@@ -173,6 +397,7 @@ app.get('/bet',auth,(req,res)=>{
     user:req.session.user.username,
     contact:req.session.user.email,
     tokens:req.session.user.total_chips,
+    session: req.query.session
   });
 });
 
--- Code_Components/src/resources/js/blackjack.js
+++ Code_Components/src/resources/js/blackjack.js
@@ -6,8 +6,120 @@ var hiddenCard;
 var deck;
 var canHit = true;
 
+// Documenting everything I've done today (7/16) because it's a real clusterfuck and I know I won't remember how it works if I don't do this now
+
+// Global variable so we don't have to fuck around with parameters for every function or whatever
+// (Now that I think about it the way I've implemented it we wouldn't need that but w h a t e v e r)
+var global_session_id;
+
+// On loading the window, this calls the begin_session function, which calls the get_session_id function (may be unnecessary, may fix)
 window.onload = function() 
 {
+    console.log(document.getElementById("session").getAttribute("value"));
+    session = parseInt(document.getElementById("session").getAttribute("value"));
+    console.log(session);
+
+    if(!session)
+    {
+        console.log("the");
+        // Call the begin_session() function, which returns an integer.
+        session = begin_session()
+        // 3. Begin the game
+        .then(function(session_id){
+            // This console.log is for debugging. Sometimes it doesn't pass the session id??
+            console.log(session_id);
+            console.log(">>>")
+            // Save it to the global session id variable
+            global_session_id = session_id;
+            
+            // Now that this is done, start the game. 
+            //This is within the .then() method because update_session() must activate after this.
+            begin_game();
+        });
+    }
+    else
+    {
+        console.log(session);
+        global_session_id = session;
+        begin_game();
+    }
+}
+    
+    // begin_session function. This is async because it needs to wait for the fetch() command within it
+    // (.then() wasn't really working right, I couldn't quite figure out how to resolve things properly)
+    async function begin_session()
+    {
+        console.log("beginning");
+        // Call the begin_session endpoint, which does not return anything
+        // (This may change later)
+        session = fetch('/table/begin_session', {method:'POST'})
+        // Wait for this above Promise to resolve.
+        await session;
+    
+        console.log("doisahd");
+        // Now set up session_id as a Promise, which will call the get_session_id() function
+        session_id = new Promise((resolve, reject) => {
+            get_session_id()
+                // If get_session_id returns a valid number, then resolve the session_id Promise as that
+                .then(function(res) {
+                    resolve(res)
+                })
+                // Otherwise, set it as the error
+                .catch(function(err) {
+                console.log(err);
+                reject(err)
+                })
+          })
+    
+          console.log("sadkl");
+        // Return the (hopefully) resolved session_id
+          return session_id;
+    }
+    
+    // This function is also async for similar reasons.
+    async function get_session_id()
+    {
+        // Set up a Promise which requests the get_curren_session endpoint
+        response = new Promise((resolve, reject) => {
+            fetch('/table/get_current_session') // Change this when the login thing is working
+                // This returns a Promise, so you have to have this here to convert it to a JSON.
+                .then(function(data)
+                {
+                    return data.json();
+                })
+                // Then resolve the Promise with the proper session id
+                .then(function(data) {
+                    document.getElementById("session").setAttribute("value", data.session_id);
+                    document.getElementById("session_id").setAttribute("value", data.session_id);
+                    resolve(data.session_id);
+                })
+                // Otherwise, set reject the Promise as the error
+                .catch(function(err)
+                {
+                    console.log(err);
+                    reject("Error");
+                })
+        })
+    
+        // Wait for the above Promise to resolve
+        await response;
+        //console.log(response);
+
+        // Add that to the HTML
+        //document.getElementById("session").setAttribute("value", response);
+        //document.getElementById("session_id").setAttribute("value", response);
+    
+        // Return the (hopefully) resolved session id.
+        return response;
+    }
+    
+// Async because the begin_session stuff has to activate first.
+async function begin_game()
+{
+    console.log("adsds")
+    await global_session_id; 
+    update_session(global_session_id);
+
     createDeck();
     shuffleDeck();
     startHand();
@@ -15,6 +127,7 @@ window.onload = function()
 
 function createDeck() 
 {
+    console.log("creating");
     let suits = ["C", "D", "H", "S"];
     let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
     deck = [];
@@ -74,6 +187,19 @@ function startHand()
         document.getElementById("hit").addEventListener("click", hit);
         document.getElementById("stay").addEventListener("click", stay);
     }
+}
+
+
+function update_session()
+{
+    // console.log(session_id)
+    fetch('/table/update_session', {
+        method:'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({session_id: global_session_id})
+    })
 }
 
 function stay()
